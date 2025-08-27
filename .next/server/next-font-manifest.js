532b8f617522e4421dec2de51880847d31ea4657
more0210 <michael.ore.dev@gmail.com>
2025-08-26 22:06:23 -0400

    ignore ./next

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/more/GIT/bcs/src/app/layout\":[\"static/media/c7f848bed9dd25a3-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
